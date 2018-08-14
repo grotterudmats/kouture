3d937e94b641bd9053bef8983296e4caa72b68fd
Martin Hörnquist <martin@101creative.no>
2018-08-14 11:08:27 +0200

    And back to startin

--- wp-content/plugins/frontmiintoimport/hooks.php
+++ wp-content/plugins/frontmiintoimport/hooks.php
@@ -65,6 +65,5 @@ add_action('save_post_product', function ($product_id, $product, $update) {
 
     $edited = array_unique($edited);
 
-    return;
     update_post_meta($product_id, 'edited', json_encode($edited));
 }, 10, 3);
