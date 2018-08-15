2e0abe3f1e31ddbdc53b15b6b3b0280015b1121d
Martin Hörnquist <martin@101creative.no>
2018-08-15 11:01:55 +0200

    Let's try and att a settings field

--- wp-content/plugins/frontmiintoimport/plugin.php
+++ wp-content/plugins/frontmiintoimport/plugin.php
@@ -6,6 +6,16 @@ Plugin Name:  Front Systems + Miinto Product Import
  */
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
-require_once(__DIR__ . "/hooks.php");
+//require_once(__DIR__ . "/hooks.php");
+
+
+add_action('admin_init', function() {
+  register_setting('front_settings', 'front_username', array(
+    'type' => string,
+    'description' => 'Front Systems Username',
+    'sanitize_callback' => 'sanitize_text_field',
+    'default' => NULL,
+  ));
+});
 
 ?>
