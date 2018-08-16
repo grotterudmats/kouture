d9229faaa1063aab3b06dc88629e02649d4bf3fe
Martin Hörnquist <martin@101creative.no>
2018-08-16 11:56:26 +0200

    Maybe images work now

--- wp-content/plugins/frontmiintoimport/hooks/woocommerce.rest.api.php
+++ wp-content/plugins/frontmiintoimport/hooks/woocommerce.rest.api.php
@@ -12,27 +12,27 @@ function custom_change_product_response( $response, $object, $request ) {
       $variation_id                    = $variation;
       $variation                       = new WC_Product_Variation( $variation_id );
       $variation_attributes            = $variation->get_variation_attributes();
-      $variations_res = array_merge($variation_res, $variation->get_data());
-      /*
       $variations_res['variation_id']  = $variation_id;
       $variations_res['on_sale']       = $variation->is_on_sale();
       $variations_res['regular_price'] = (float) $variation->regular_price;
       $variations_res['sale_price']    = (float) $variation->sale_price;
       $variations_res['currency']      = get_woocommerce_currency_symbol();
       $variations_res['sku']           = $variation->get_sku();
-      $variations_res['images']        = $variation->get_images();
       $variations_res['description']   = $variation->get_description();
       $variations_res['quantity']      = $variation->get_stock_quantity();
       if ( $variations_res['quantity'] == null ) {
         $variations_res['quantity'] = '';
       }
       $variations_res['in_stock']        = $variation->is_in_stock();
-       */
-      $featured_image                    = wp_get_attachment_image_src( get_post_thumbnail_id( $variation_id ), 'shop_catalog' );
-      $variations_res['variation_image'] = $featured_image[0];
-      if ( $variations_res['variation_image'] == null ) {
-        $variations_res['variation_image'] = " ";
+      $images = array();
+      foreach($variation->get_gallery_image_ids() as $index=>$image_id) {
+        $images[] = array(
+          'id' => $image_id,
+          'position' => $index,
+          'src' => wp_get_attachment_image_src($image_id),
+        );
       }
+      $variations_res['images'] = $images;
       foreach ( $variation_attributes as $key => $value ) {
         if ( $key == 'attribute_pa_color' ) {
           $variation_attributes[ $key ] = get_color_code_by_name( $value );
