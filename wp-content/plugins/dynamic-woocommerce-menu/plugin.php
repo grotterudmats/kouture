579db5c2bd1a445215b04930cdb939b18ffb3857
Martin Hörnquist <martin@101creative.no>
2018-08-24 11:38:03 +0200

    Dynamic menu pretty much works

--- wp-content/plugins/dynamic-woocommerce-menu/plugin.php
+++ wp-content/plugins/dynamic-woocommerce-menu/plugin.php
@@ -36,12 +36,73 @@ add_filter("wp_get_nav_menu_items", function ($items, $menu, $args) {
 
   $brands = get_terms('product_brand');
   $categories = get_terms('product_cat');
+  $shoes = end(array_filter($categories, function($cat) {
+    return strtolower($cat->name) == 'sko';
+  }));
+  $accessories = end(array_filter($categories, function($cat) {
+    return strtolower($cat->name) == 'accessories';
+  }));
+  $categories = array_filter($categories, function($cat) {
+    return strtolower($cat->name) != 'accessories' &&
+      strtolower($cat->name) != 'sko';
+  });
 
 	$order = ($items[sizeof($items)-1]->ID)+1;
   foreach($items as $index => $item) {
+    if($item->title == 'Produkter') {
+      $subs = array_filter($categories, function($cat) {
+        return $cat->parent == 0;
+      });
+      foreach($subs as $sub) {
+        $new = _create_menu_items($sub->name, get_term_link($sub), $order, $item->ID);
+        $items[] = $new;
+        $order++;
+			  $children = get_terms( array('taxonomy' => 'product_cat', 'parent'  => $sub->term_id ) );
+        $parent_id = $new->ID;
+        if(!empty($children)) {
+          foreach($children as $child) {
+            $new = _create_menu_items($child->name, get_term_link($child), $order, $parent_id);
+            $items[] = $new;
+            $order++;
+          }
+        }
+      }
+    }
+    if($item->title == 'Sko') {
+		  $subs = get_terms( array('taxonomy' => 'product_cat', 'parent'  => $shoes->term_id ) );
+      foreach($subs as $sub) {
+        $new = _create_menu_items($sub->name, get_term_link($sub), $order, $item->ID);
+        $items[] = $new;
+        $order++;
+			  $children = get_terms( array('taxonomy' => 'product_cat', 'parent'  => $sub->term_id ) );
+        if(!empty($children)) {
+          foreach($children as $child) {
+            $new = _create_menu_items($child->name, get_term_link($child), $order, $new->ID);
+            $items[] = $new;
+            $order++;
+          }
+        }
+      }
+    }
+    if($item->title == 'Tilbehør') {
+		  $subs = get_terms( array('taxonomy' => 'product_cat', 'parent'  => $accessories->term_id ) );
+      foreach($subs as $sub) {
+        $new = _create_menu_items($sub->name, get_term_link($sub), $order, $item->ID);
+        $items[] = $new;
+        $order++;
+			  $children = get_terms( array('taxonomy' => 'product_cat', 'parent'  => $sub->term_id ) );
+        if(!empty($children)) {
+          foreach($children as $child) {
+            $new = _create_menu_items($child->name, get_term_link($child), $order, $new->ID);
+            $items[] = $new;
+            $order++;
+          }
+        }
+      }
+    }
     if($item->title == 'Merker') {
       foreach($brands as $brand) {
-        $new = _create_menu_items($brand->name, "/brand/" . $brand->slug, $order, $item->ID);
+        $new = _create_menu_items($brand->name, get_term_link($brand), $order, $item->ID);
         $items[] = $new;
         $order++;
       }
